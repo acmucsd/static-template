eea33cc7bfcdd3c80fc5c377ebeb76fc6fa40134
StoneT2000 <stonezt2019@gmail.com>
2020-07-05 14:06:05 -0700

    template component for copy and pasting

---
+++ src/components/_Template/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.less';
+
+const Template: React.FC = () => {
+  return (
+    <div className="Template">
+      <section>
+        
+      </section>
+    </div>
+  );
+};
+
+export default Template;
