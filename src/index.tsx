392d26eff839537043288fecbdfba5e11715688b
StoneT2000 <stonezt2019@gmail.com>
2020-07-08 22:18:57 -0700

    removed less imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import 'styles/index.less';
 import Home from "pages/Home";
 import Standard from 'pages/Standard';
 import Contact from "components/Contact";
