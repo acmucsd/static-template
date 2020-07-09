30c4d899cc8395de819ce3df7284af3a239b6f8b
StoneT2000 <stonezt2019@gmail.com>
2020-07-08 22:16:10 -0700

    fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/style.less";
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from "pages/Home";
